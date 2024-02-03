325352be785c683342201ad1f9aeaf8c9853d504
luisdantuono <148562447+luisdantuono@users.noreply.github.com>
2024-02-03 18:26:09 -0400

    Course

---
+++ src/main/Models/Course.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+
+public class Course {
+    String name;
+    
+    public Course(String name){
+        this.name=name;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+}
