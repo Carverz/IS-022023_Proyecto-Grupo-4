4a577c2724f5598c17638c43115c57e091728a58
luisdantuono <148562447+luisdantuono@users.noreply.github.com>
2024-02-04 18:53:40 -0400

    Update HomeController.java

--- src/main/HomeController.java
+++ src/main/HomeController.java
@@ -7,9 +7,12 @@ public class HomeController {
     private User usuario;
     private Course[] curso;
     
-    public HomeController(User usuario, Course[] curso){
+    public HomeController(User usuario){
         this.usuario = usuario;
-       this.curso = curso;
+        this.curso = new Course[3];
+                curso[0] = new Course("Lenguaje de Programación Java: Nivel Basico");
+                curso[1] = new Course("Lenguaje de Programación C++: Nivel Basico");
+                curso[2] = new Course("Lenguaje de Programación PHP: Nivel Basico");
     }
     
     public String getName(int i){
