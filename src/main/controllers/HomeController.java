d3ed31ac852372628e57e23765354eba58fbfbaf
luisdantuono <148562447+luisdantuono@users.noreply.github.com>
2024-02-22 02:28:01 -0400

    Update HomeController.java

--- src/main/controllers/HomeController.java
+++ src/main/controllers/HomeController.java
@@ -2,10 +2,14 @@
 package main.controllers;
 import main.models.Course;
 import main.models.User;
+import main.models.Answer;
+import main.models.Question;
+import main.models.Exam;
 
 public class HomeController {
     public User usuario;
     private Course[] curso;
+    private TakeExamController examController;
     
     public HomeController(User usuario){
         this.usuario = usuario;
@@ -13,6 +17,14 @@ public class HomeController {
                 curso[0] = new Course("Lenguaje de Programación Java: Nivel Basico");
                 curso[1] = new Course("Lenguaje de Programación C++: Nivel Basico");
                 curso[2] = new Course("Lenguaje de Programación PHP: Nivel Basico");
+    }
+
+    public TakeExamController getExamController(String name){
+        if(name=="Exam1"){
+            
+            
+        }
+        
     }
     
   
