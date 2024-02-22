e1ffda19ae3a1455c48467826ef0b2c017f463b0
Carlos Zavarce <cezavarcev@hotmail.com>
2024-02-22 02:21:11 -0400

    ProfileControllerTest Modificacion

--- src/test/ProfileControllerTest.java
+++ src/test/ProfileControllerTest.java
@@ -8,33 +8,9 @@ import main.controllers.ProfileController;
 public class ProfileControllerTest {
 
     @Test
-    public void testGetName() {
-        // Arrange
-        User user = new User("Carlos Zavarce", "cezavarcev16@gmail.com", "password", new int[]{0, 1, 2});
-        ProfileController controller = new ProfileController(user);
-
-        // Act & Assert
-        assertEquals("Lenguaje de Programación Java: Nivel Basico", controller.getName(0));
-        assertEquals("Lenguaje de Programación C++: Nivel Basico", controller.getName(1));
-        assertEquals("Lenguaje de Programación PHP: Nivel Basico", controller.getName(2));
-    }
-
-    @Test
-    public void testGetName_InvalidIndex() {
-        // Arrange
-        User user = new User("Carlos Zavarce", "cezavarcev16@gmail.com", "password", new int[]{-1, -1, -1});
-        ProfileController controller = new ProfileController(user);
-
-        // Act & Assert
-        assertNull(controller.getUserName());
-        assertNull(controller.getUserName());
-        assertNull(controller.getUserName());
-    }
-
-    @Test
     public void testGetUserName() {
         // Arrange
-        User user = new User("Carlos Zavarce", "cezavarcev16@gmail.com", "password", new int[]{0, 1, 2});
+        User user = new User("Carlos Zavarce", "cezavarcev16@gmail.com", "password");
         ProfileController controller = new ProfileController(user);
 
         // Act & Assert
@@ -44,7 +20,7 @@ public class ProfileControllerTest {
     @Test
     public void testGetUserMail() {
         // Arrange
-        User user = new User("Carlos Zavarce", "cezavarcev16@gmail.com", "password", new int[]{0, 1, 2});
+        User user = new User("Carlos Zavarce", "cezavarcev16@gmail.com", "password");
         ProfileController controller = new ProfileController(user);
 
         // Act & Assert
