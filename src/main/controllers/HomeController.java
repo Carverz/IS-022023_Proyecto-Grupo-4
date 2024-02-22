37f301ee056c08b98b338ba99a744b42b505cb5a
luisdantuono <148562447+luisdantuono@users.noreply.github.com>
2024-02-22 02:57:54 -0400

    Update HomeController.java

--- src/main/controllers/HomeController.java
+++ src/main/controllers/HomeController.java
@@ -5,6 +5,8 @@ import main.models.User;
 import main.models.Answer;
 import main.models.Question;
 import main.models.Exam;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeController {
     public User usuario;
@@ -21,10 +23,21 @@ public class HomeController {
 
     public TakeExamController getExamController(String name){
         if(name=="Exam1"){
-            
-            
+            ArrayList<Answer> respuesta = new ArrayList<>();
+            respuesta.add(new Answer("5",false));
+            respuesta.add(new Answer("6",false));
+            respuesta.add(new Answer("4",false));
+            respuesta.add(new Answer("7",false));
+            respuesta.add(new Answer("8",false));
+
+            ArrayList<Question> pregunta = new ArrayList<>();
+            pregunta.add(new Question("¿Cuanto es 2 + 2?",respuesta,"2 + 2 = 4","Algebra"));
+
+            Exam examen = new Exam(pregunta,120,name);
+            examController = new TakeExamController(examen,usuario);
+            return ExamController;
         }
-        
+        return null;
     }
     
   
